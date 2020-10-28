554b437d1417552ad659bf09cbf1dfd48f564fa7
Uliana Utina <ulyana190909@yandex.ru>
2020-10-28 21:39:54 +0300

    доработка теста

--- src/test/java/ru/netology/domain/СardDeliveryOrderTest.java
+++ src/test/java/ru/netology/domain/СardDeliveryOrderTest.java
@@ -2,24 +2,49 @@ package ru.netology.domain;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class СardDeliveryOrderTest {
 
     @Test
+    void setDayDelivery(){
+        Calendar calendar = new GregorianCalendar();
+        Date date = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_MONTH, 3);
+        }
+
+
+    @Test
+    void shouldSendValidRequestWithCalendar() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] .input__control").setValue("Красноярск");
+        $("[data-test-id='date'] .input__control").setValue(setDayDelivery(););
+        $("[data-test-id='name'] .input__control").setValue("Малышев Петр");
+        $("[data-test-id='phone'] .input__control").setValue("+78542586320");
+        $("[data-test-id='agreement']").click();
+        $$("[type='button']").find(exactText("Забронировать")).click();
+        $(byText("Встреча успешно забронирована на")).waitUntil(visible, 15000);
+    }
+
+    @Test
     void shouldSendValidRequest() {
         open("http://localhost:9999");
-        $("[data-test-id='city']").setValue("Красноярск");
-        $("[data-test-id='date']").setValue("05.11.2020");
-        $("[data-test-id='name']").setValue("Малышев Петр");
-        $("[data-test-id='phone']").setValue("+78542586320");
+        $("[data-test-id='city'] .input__control").setValue("Красноярск");
+        $("[data-test-id='date'] .input__control").setValue("31.10.2020");
+        $("[data-test-id='name'] .input__control").setValue("Малышев Петр");
+        $("[data-test-id='phone'] .input__control").setValue("+78542586320");
         $("[data-test-id='agreement']").click();
-        $$("button__text").find(exactText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована на 05.11.2020")).waitUntil(visible, 15000);
+        $$("[type='button']").find(exactText("Забронировать")).click();
+        $(byText("Встреча успешно забронирована на")).waitUntil(visible, 15000);
     }
 }
